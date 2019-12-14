fda65d71f21105c821399f8e5093a36104511233
Losur <57538616+Losur@users.noreply.github.com>
2019-12-14 03:10:19 +0300

    Add files via upload
    That's what we have right now

---
+++ Mopve.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mopve : MonoBehaviour
+{
+    private Touch touch;
+
+    private Vector2 touchPosition;
+
+    private Quaternion rotationY;
+
+    private Quaternion rotationX;
+
+    private float rotateSpeedModifer = 0.01f;
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Moved)
+            {
+                rotationY = Quaternion.Euler(
+                    0f,
+                    - touch.deltaPosition.x * rotateSpeedModifer,
+                    0f);
+
+                transform.rotation = rotationY * transform.rotation;
+            }
+        }
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                rotationX = Quaternion.Euler(
+                    - touch.deltaPosition.y * rotateSpeedModifer,
+                    0f,
+                    0f);
+
+                transform.rotation = rotationX * transform.rotation;
+            }
+        }         
+    }
+}
